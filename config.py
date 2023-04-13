3ed23b2964ee0831d206b59ceb6a8a256b11675b
Jos Verlinde <jos_verlinde@hotmail.com>
2023-04-13 20:24:04 +0200

    update config
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- config.py
+++ config.py
@@ -2,45 +2,57 @@ from pathlib import Path
 
 commands = {
     "list": {
-        "command": ["connect","list"],
+        "command": ["connect", "list"],
         "help": "List serial devices",
     },
     "run script='main.py'": {
         "command": ["exec", "exec( open(script).read() , globals() )"],
         "help": "Run a script on the remote device",
     },
-    "md": {"command": "mkdir", "help": "alias for mkdir" },
-    "del": {"command": "rm", "help": "alias for rm" },
-    "dir": {"command": "ls", "help": "alias for ls" },
-    "copy": {"command": "cp", "help": "alias for cp" },
+    "md": {"command": "mkdir", "help": "alias for mkdir"},
+    "del": {"command": "rm", "help": "alias for rm"},
+    "dir": {"command": "ls", "help": "alias for ls"},
+    "copy": {"command": "cp", "help": "alias for cp"},
 }
 
 # register com ports in both upper and lower case
-for port_num in range(1,30):
+for port_num in range(1, 30):
     prefix, port = ("com", "COM")
     commands["{}{}".format(prefix, port_num)] = {
         "command": "connect {}{}".format(port, port_num),
-        "help": 'connect to serial port {}{}:'.format(port, port_num),
+        "help": "connect to serial port {}{}:".format(port, port_num),
     }
     prefix, port = ("COM", "COM")
     commands["{}{}".format(prefix, port_num)] = {
         "command": "connect {}{}".format(port, port_num),
-        "help": 'connect to serial port {}{}:'.format(port, port_num),
+        "help": "connect to serial port {}{}:".format(port, port_num),
     }
 
-here  = Path(__file__).parent.absolute()
+here = Path(__file__).parent.absolute()
 
 files = (here / "snippets").glob("*.py")
 for file in files:
-    with open(file, encoding="utf-8") as f:
-        help = f.readline()
+    try:
+        with open(file, encoding="utf-8") as f:
+            help = f.readline()
+            params = f.readline()
+    except UnicodeDecodeError:
+        continue
     if help[0] in ("#", '"'):
         help = help[1:].strip().strip('"').strip()
         # help = f"{help} - {file.absolute()}"
         help = f"{help} - {file.relative_to(here)}"
     else:
-        help = file.absolute()
-    commands[file.stem] = {
+        help = ""
+    command = file.stem
+    # BUG: Quote handling from commandline --> mpremote.main:do_command_expansion(args): prevents handling string parameters 
+    # if command == "wipe_folder":
+    #     command = "wipe_folder folder='/'"
+    # if params[0] not in ("#", '"') and '=' in params:
+    #     params = params.strip()
+    #     command += f" {params}"
+
+    commands[command] = {
         "command": ["exec", file.read_text()],
         "help": help,
     }
--- snippets/wipe_folder.py
+++ snippets/wipe_folder.py
@@ -1,29 +1,33 @@
-#Clean all files from flash 
-# use with care ; there is no undo or trashcan 
+# command = "wipe_folder folder='/'"
+# Clean all files from flash ,use with care ; there is no undo or trashcan 
+import uos as os
 
-import uos as os 
+if not folder: # Default to root # type: ignore
+    folder = '/'
 
-def wipe_dir( path=".",sub=True):
-        print( "wipe path {}".format(path) )
-        l = os.listdir(path)
-        l.sort()
-        #print(l)
-        if l != ['']:
-            for f in l:
-                child = "{}/{}".format(path, f)
-                #print(" - "+child)
-                st = os.stat(child)
-                if st[0] & 0x4000:  # stat.S_IFDIR
-                    if sub:
-                        wipe_dir(child,sub)
-                        try:
-                            os.rmdir(child)
-                        except:
-                            print("Error deleting folder {}".format(child))
-                else: # File
-                    try:  
-                        os.remove(child)
-                    except:
-                        print("Error deleting file {}".format(child))
 
-wipe_dir(path='/')
+def wipe_folder( folder=".",sub=True):  # sourcery skip: use-fstring-for-formatting
+    print( "wipe path {}".format(folder) )
+    l = os.listdir(folder)
+    l.sort()
+    #print(l)
+    # if l != ['']:
+    for f in l:
+        if f:
+            child = "{}/{}".format(folder, f)
+            print(" - ", child)
+            st = os.stat(child)
+            if st[0] & 0x4000:  # stat.S_IFDIR
+                if sub:
+                    wipe_folder(child,sub)
+                    try:
+                        os.rmdir(child)
+                    except OSError:
+                        print("Error deleting folder {}".format(child))
+            else: # File
+                try:  
+                    os.remove(child)
+                except OSError:
+                    print("Error deleting file {}".format(child))
+
+wipe_folder(folder=folder)
