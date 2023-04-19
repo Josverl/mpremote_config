cb8879861efe6c1bdff0737fd7701fae76d22094
Jos Verlinde <jos_verlinde@hotmail.com>
2023-04-19 19:36:38 +0200

    update getport prototype
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- snippets/get-port.py
+++ snippets/get-port.py
@@ -9,6 +9,14 @@ except ImportError:
     from ucollections import OrderedDict  # type: ignore
 
 
+def file_exists(filename: str):
+    try:
+        os.stat(filename)
+        return True
+    except OSError:
+        return False
+
+
 def _info():  # type:() -> dict[str, str]
     info = OrderedDict(
         {
@@ -39,26 +47,30 @@ def _info():  # type:() -> dict[str, str]
         )
     except (AttributeError, IndexError):
         pass
-
     gc.collect()
     try:
         # look up the board name in the board_info.csv file
-        b = info["board"].strip()
-        with open("board_info.csv", "r") as f:
-            if not find_board(info, b, f):
+        for filename in ["board_info.csv", "lib/board_info.csv"]:
+            if file_exists(filename):
+                b = info["board"].strip()
+                if find_board(info, b, filename):
+                    break
                 if "with" in b:
-                    f.seek(0)
                     b = b.split("with")[0].strip()
-                    find_board(info, b, f)
-        info["board"] = info["board"].replace(" ", "_")
+                    if find_board(info, b, filename):
+                        break
     except (AttributeError, IndexError, OSError):
         pass
+    info["board"] = info["board"].replace(" ", "_")
     gc.collect()
 
-    try:
-        info["build"] = sys.version.split(";")[1].strip().split(" ")[1].split("-")[1]
+    try:  # extract buildfrom uname().version if available
+        info["build"] = os.uname()[3].split(" on ")[0].split("-")[1]
     except (AttributeError, IndexError):
-        pass
+        try:  # extract build from sys.version if available
+            info["build"] = sys.version.split(";")[1].strip().split(" ")[1].split("-")[1]
+        except (AttributeError, IndexError):
+            pass
 
     if info["version"] == "" and sys.platform not in ("unix", "win32"):
         try:
@@ -105,17 +117,23 @@ def _info():  # type:() -> dict[str, str]
         if arch:
             info["arch"] = arch
         # .mpy version.minor
-        info["mpy"] = "v{}.{}".format(sys_mpy & 0xff, sys_mpy >> 8 & 3)
+        info["mpy"] = "v{}.{}".format(sys_mpy & 0xFF, sys_mpy >> 8 & 3)
+        # extract_os_info(info)
     return info
 
 
-def find_board(info, board_descr, f):
-    for line in f:
-        descr_, board_ = line.split(",")[0].strip(), line.split(",")[1].strip()
-        if descr_ == board_descr:
-            info["board"] = board_
-            # info["board_d"] = descr_
-            return True
+def find_board(info: dict, board_descr: str, filename: str):
+    "Find the board in the board_info.csv file"
+    with open(filename, "r") as file:
+        # ugly code to make testable in python and micropython
+        while 1:
+            line = file.readline()
+            if not line:
+                break
+            descr_, board_ = line.split(",")[0].strip(), line.split(",")[1].strip()
+            if descr_ == board_descr:
+                info["board"] = board_
+                return True
     return False
 
 
@@ -123,3 +141,5 @@ info = _info()
 # output the info dict as a string with the OrderedDict() removed
 repr_info = str(info).replace("OrderedDict(", "").rstrip(")")
 print(repr_info)
+
+print("{family}-{version}-{port}-{board}".format(**info))
