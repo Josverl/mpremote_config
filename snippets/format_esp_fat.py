e304ef38d5f49892de1bd616455272eac4b9899a
Jos Verlinde <jos_verlinde@hotmail.com>
2023-04-25 16:17:51 +0200

    correct snippet  format_esp_lfs2.py and mount Fixes #4
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- snippets/format_esp_fat.py
+++ snippets/format_esp_fat.py
@@ -1,6 +1,12 @@
 "format/erase an ESPxx with the FAT filesystem"
 import os
 
-os.umount('/')
+if 'mount_point' not in globals():
+    mount_point = '/flash'
+
+os.umount(mount_point)
 os.VfsFat.mkfs(bdev)
-os.mount(bdev, '/')
+os.mount(bdev, mount_point, readonly=False)
+
+print(mount_point, os.listdir(mount_point))
+
--- snippets/format_esp_lfs2.py
+++ snippets/format_esp_lfs2.py
@@ -1,6 +1,11 @@
 # format/erase an ESPxx with the lfs2 filesystem
 import os
 
-os.umount('/')
-os.VfsFat.mkfs(bdev)
-os.mount(bdev, '/')
+if 'mount_point' not in globals():
+    mount_point = '/flash'
+    
+os.umount(mount_point)
+os.VfsLfs2.mkfs(bdev, readsize=32, progsize=32, lookahead=32)
+os.mount(bdev, mount_point, readonly=False)
+
+print(mount_point, os.listdir(mount_point))
